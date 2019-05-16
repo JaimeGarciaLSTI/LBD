ac643445c18e957815d8fc83c58c0063cdc5c65d
JaimeGarciaLSTI <42558400+JaimeGarciaLSTI@users.noreply.github.com>
2019-05-16 18:51:27 -0500

    Add files via upload

--- Practica6.sql
+++ Practica6.sql
@@ -1,51 +1,42 @@
--- Selects
-SELECT idClientes as 'ID Chido', Nombre as 'Nombre', ApellidoP as 'ApellidoP',
-ApellidoM as 'ApellidoM',(Edad) as 'Añejos' 
-from practicas.clientes;
+Select *
+from Cliente
+Where ApPaterno_c = 'Castillo' AND ApMaterno_c = 'Guajardo'
 
-SELECT idClientes2 as 'ID Chido2', Nombre as 'Nombre', ApellidoP as 'ApellidoP',
-ApellidoM as 'ApellidoM',(Edad) as 'Soles vividos' 
-from practicas.clientes2;
 
-SELECT idClientes3 as 'ID Chido3', Nombre as 'Nombre', ApellidoP as 'ApellidoP',
-ApellidoM as 'ApellidoM',(Edad) as 'Vida' 
-from practicas.clientes3;
+Select COUNT (Pedido_code)
+from Pedido
+where Platillo_code >1 OR Postre_code >5
 
-SELECT idClientes4 as 'ID Chido4', Nombre as 'Nombre', ApellidoP as 'ApellidoP',
-ApellidoM as 'ApellidoM',(Edad) as 'Menos Vida' 
-from practicas.clientes4;
 
-SELECT idClientes5 as 'ID Chido5', Nombre as 'Nombre', ApellidoP as 'ApellidoP',
-ApellidoM as 'ApellidoM',(Edad) as 'Dias sin morir' 
-from practicas.clientes5;
+Select Cliente.Nombre_c,Reservacion.Reservacion_code
+from Reservacion
+inner join  Cliente on Reservacion.Cliente_code = Reservacion.Cliente_code
+where Nombre_c = '%O%'
 
-SELECT idClientes6 as 'ID Chido6', Nombre as 'Nombre', ApellidoP as 'ApellidoP',
-ApellidoM as 'ApellidoM',(Edad) as 'Años' 
-from practicas.clientes6;
+Select Cliente.Nombre_c + Cliente.ApPaterno_c + Cliente.ApMaterno_c as NOMBRE_COMPLETO
+from Cliente
+where Cliente_code = 2
 
-SELECT idClientes7 as 'ID Chido7', Nombre as 'Nombre', ApellidoP as 'ApellidoP',
-ApellidoM as 'ApellidoM',(Edad) as 'Años' 
-from practicas.clientes7;
+Select Info_Gral.Correo
+from Info_Gral 
 
-SELECT idClientes8 as 'ID Chido8', Nombre as 'Nombre', ApellidoP as 'ApellidoP',
-ApellidoM as 'ApellidoM',(Edad) as 'Años' 
-from practicas.clientes8;
 
-SELECT idClientes9 as 'ID Chido9', Nombre as 'Nombre', ApellidoP as 'ApellidoP',
-ApellidoM as 'ApellidoM',(Edad) as 'Años' 
-from practicas.clientes9;
+Select Cliente.Nombre_c as Nombre, Reservacion_code as Reservacion
+from Cliente
+inner join Reservacion on Cliente.Cliente_code = Reservacion.Reservacion_code
 
-SELECT idClientes10 as 'ID Chido10', Nombre as 'Nombre', ApellidoP as 'ApellidoP',
-ApellidoM as 'ApellidoM',(Edad) as 'Años'
-from practicas.clientes10;
+Select Ticket.Pago_code
+from Ticket
 
--- Drop
-DROP table practicas.clientes7; -- No utilizar
+Select Cliente.Nombre_c, Ticket.Ticket_code
+from Ticket, Reservacion
+inner join Cliente on Reservacion.Cliente_code = Cliente.Cliente_code
 
--- Insert INTO
-insert into clientes(idClienteses,Nombre,ApellidoP,ApellidoM,Edad)
-values('0000','Jhonny',' Be','Good','31');
+Select Postre.Descripcion
+from Postre
+WHERE Postre.Postre_code= 4
 
--- Insert INTO select
-INSERT INTO practicas.clienteses10 SELECT * FROM practicas.clientes;
-SELECT * FROM practicas.clientes10;
+Select Cliente.ApMaterno_c + Cliente.ApPaterno_c + Cliente.Nombre_c AS NOMBRE_COMPLETO
+from Cliente
+where ApMaterno_c like '%Castillo%'
+
